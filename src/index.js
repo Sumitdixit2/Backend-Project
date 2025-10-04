ddfbae98683b6d408cff6229ac1982dc1d53d480
Sumitdixit2 <sumitdixit00007@gmail.com>
2025-10-04 12:40:52 +0530

    Added Essential Utils

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js"
 dotenv.config
-connectDB();
+
+const Listen = async() => {
+    try {
+       await connectDB()
+
+        app.use((err) => {
+            console.log("There seems to be an error: ",err);
+            throw new Error
+        })
+
+        app.listen(process.env.PORT || 5000,() => {
+            console.log("App listening on Port : ",process.env.PORT);
+        })
+    } catch (error) {
+        console.error("Error at calling the DB function: ",error);
+    }
+}
+
+Listen();
+
+
